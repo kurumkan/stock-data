e8b766ec89feb25bbc4afa250cd717bbb87b145d
kurumkan <joomrise@gmail.com>
2017-02-27 11:35:25 +0800

    add gen-random-colors support

--- frontend/src/actions/Actions.jsx
+++ frontend/src/actions/Actions.jsx
@@ -1,3 +1,5 @@
+import {generateColor} from 'gen-random-colors'
+
 export function addStockRemote(stock){			
 	stock.color = generateColor(3);
 	return {
@@ -56,11 +58,3 @@ export function removeError(){
 	}
 }
 
-//generate random color with contolled brightness
-function generateColor(brightness){
-	// Six levels of brightness from 0 to 5, 0 being the darkest
-	var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
-    var mix = [brightness*51, brightness*51, brightness*51]; //51 => 255/5
-    var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x){ return Math.round(x/2.0)})
-    return "rgb(" + mixedrgb.join(",") + ")";
-}
